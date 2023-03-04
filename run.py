c7dba7d2c1e23aa09e67253a4944419b6ff4ad4f
green-nisaba <basilandsalt@gmail.com>
2023-03-04 18:24:48 +0000

    Add spaces to the board

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ board = ["-", "-", "-",
 
 # Creating a board
 def displayBoard(board):
-    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[0] + " | " + board[1] + " | " + board[2])
     print("-" * 9)
-    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[3] + " | " + board[4] + " | " + board[5])
     print("-" * 9)
-    print(board[6] + "|" + board[7] + "|" + board[8])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
+displayBoard(board)
