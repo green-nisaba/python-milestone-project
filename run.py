14745659bab72e73a414b4d6d481b5cda663777b
green-nisaba <basilandsalt@gmail.com>
2023-03-11 17:23:19 +0000

    Define global virables

--- run.py
+++ run.py
@@ -2,13 +2,17 @@ board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
 
+current_player = "X"
+winner = None
+game_going = True
+
 
 # Creating a board
-def displayBoard(board):
+def display_board(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
     print("-" * 9)
     print(board[3] + " | " + board[4] + " | " + board[5])
     print("-" * 9)
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-displayBoard(board)
+
