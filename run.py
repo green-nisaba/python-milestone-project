7793f5bac67c0477f457c79fdaefab8da7b53bc6
green-nisaba <basilandsalt@gmail.com>
2023-03-11 17:57:39 +0000

    Add function to check for winner in rows

--- run.py
+++ run.py
@@ -34,10 +34,22 @@ def horizontal_winner(board):
     elif board[3] == board[4] == board[5] and board[3] != "-":
         winner = board[0]
         return True
-     elif board[6] == board[7] == board[8] and board[6] != "-":
+    elif board[6] == board[7] == board[8] and board[6] != "-":
         winner = board[6]
         return True
-  
+
+
+def row_winner(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif  board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif  board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
 
 # 
 while game_going:
