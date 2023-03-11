1ae0d5fbc8e2982c11c3d2d7b73ef696a51f2240
green-nisaba <basilandsalt@gmail.com>
2023-03-11 17:32:05 +0000

    Add function to handle user's imput

--- run.py
+++ run.py
@@ -15,4 +15,10 @@ def display_board(board):
     print("-" * 9)
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-
+# Receive user's input
+def players_choice(board):
+    choosen = int(input("Choose a number 1-9: "))
+    if choosen >= 1 and choosen <=9 and board[choosen - 1] == "-":
+        board[choosen - 1] = current_player
+    else:
+        print("Please choose a valid option")
